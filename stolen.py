655b06903a8b38d29db8c3f4566f01277be863f7
Galmeno <47006183+Galmeno@users.noreply.github.com>
2021-01-20 22:36:01 +0100

    Create stolen.py

---
+++ stolen.py
@@ -0,0 +1,16 @@
+async def on_message(msg):
+    with open("answers.json", 'r') as f:
+        inte = json.load(f)
+    if not msg.author.id == client.user.id:
+        if not msg.guild:
+            intents = inte['intents']
+            for intent in intents:
+                tag = intent['tag']
+                for patternz in intent['tag']:
+                    #print(intent['tag']['patterns'])
+                    if msg.content.lower() in tag['patterns']:
+                        await msg.author.send(f"{tag['name']}, {random.choice(tag['responses'])}")
+                        return
+                    else:
+                        await msg.author.send("Idek")
+                        return
